f472b4763cf6f38602fdc326a34c85a526572a34
이경우 <kyungwoo5826@naver.com>
2023-04-03 22:44:00 +0900

    init project

---
+++ src/main/java/com/baeksu/happylife/HappyLifeApplication.java
@@ -0,0 +1,13 @@
+package com.baeksu.happylife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappyLifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HappyLifeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/baeksu/happylife/HappyLifeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baeksu.happylife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HappyLifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
